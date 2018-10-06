7c5a2c0df1b86883f3165811c89179c59c04123f
Stephan09 <38815320+Stephan09@users.noreply.github.com>
2018-10-06 18:42:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,7 @@ client.on('message', message => {
 	
   
   
-client.login(token);
+client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
 
 client.on('ready', () => {
 	console.log("Bot ist fertig xD\n\n")
